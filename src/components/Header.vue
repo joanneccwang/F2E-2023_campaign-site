3915bd181fa98dcc280f33c2f796a3052c62076a
Joanne Wang <joanneccwang@gmail.com>
2023-11-16 02:44:16 +0800

    feat: layout header bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,31 @@
 <script setup lang="ts"></script>
 <template>
   <div id="header">
-    <!-- logo -->
-    <div id="header_logo">
-      <img src="../assets/icons/logo.svg" />
-      <h4 id="logo_title">喵立翰 Miao Li-Han</h4>
-    </div>
-    <!-- menu -->
-    <div id="header_menu">
-      <div class="menu_button">候選人主張</div>
-      <div class="menu_button">最新活動</div>
-      <div class="menu_button">政策議題</div>
-      <div class="menu_button">小額捐款</div>
-      <div class="menu_button">民眾服務信箱</div>
-    </div>
-    <!-- social media -->
-    <div id="header_social">
-      <div class="social_button">
-        <img src="../assets/icons/social_fb.svg" />
+    <div id="header_inner">
+      <!-- logo -->
+      <div id="header_logo">
+        <img src="../assets/icons/logo.svg" />
+        <h4 id="logo_title">喵立翰 Miao Li-Han</h4>
       </div>
-      <div class="social_button">
-        <img src="../assets/icons/social_ig.svg" />
+      <!-- menu -->
+      <div id="header_menu">
+        <div class="menu_button">候選人主張</div>
+        <div class="menu_button">最新活動</div>
+        <div class="menu_button">政策議題</div>
+        <div class="menu_button">小額捐款</div>
+        <div class="menu_button">民眾服務信箱</div>
       </div>
-      <div class="social_button">
-        <img src="../assets/icons/social_youtube.svg" />
+      <!-- social media -->
+      <div id="header_social">
+        <div class="social_button">
+          <img src="../assets/icons/social_fb.svg" />
+        </div>
+        <div class="social_button">
+          <img src="../assets/icons/social_ig.svg" />
+        </div>
+        <div class="social_button">
+          <img src="../assets/icons/social_youtube.svg" />
+        </div>
       </div>
     </div>
   </div>
@@ -31,15 +33,19 @@
 
 <style lang="scss" scoped>
 #header {
+  padding: 16px 24px;
+}
+#header_inner {
   display: flex;
   flex-direction: row;
-  border: 1px solid hotpink;
+  padding: 12px 24px;
 
-  padding: 16px 24px;
+  background: white;
+  border-radius: 12px;
+  position: sticky;
 }
 #header_logo {
   flex: 0 0 auto;
-  border: 1px solid blue;
 
   display: flex;
   flex-direction: row;
@@ -54,33 +60,35 @@
 
 #header_menu {
   flex: 1;
-  border: 1px solid green;
 
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   gap: 16px;
+
+  .menu_button {
+    padding: 8px;
+    cursor: pointer;
+    color: $text-primary;
+    font-size: $font-base;
+  }
 }
 
-.menu_button {
-  padding: 8px;
-  cursor: pointer;
-}
 #header_social {
   flex: 0 0 auto;
-  border: 1px solid orange;
 
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 16px;
-}
-.social_button {
-  padding: 2px;
-  cursor: pointer;
 
-  display: flex;
-  align-items: center;
+  .social_button {
+    padding: 2px;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
