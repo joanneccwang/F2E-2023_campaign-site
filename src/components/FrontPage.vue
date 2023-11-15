32a8a1ae32db77d710713d10ab9669bebc12e040
Wang Ching-Chia <chia@Wangs-MacBook-Pro.local>
2023-11-16 01:10:38 +0800

    init: init project

---
+++ src/components/FrontPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <section>
+    <div id="slogan">台灣的明天 喵先鋪路</div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div id="header">
+    <!-- logo -->
+    <div id="header_logo">
+      <img src="../assets/icons/logo.svg" />
+      <div id="logo_title">喵立翰 Miao Li-Han</div>
+    </div>
+    <!-- menu -->
+    <div id="header_menu">
+      <div class="menu_button">候選人主張</div>
+      <div class="menu_button">最新活動</div>
+      <div class="menu_button">政策議題</div>
+      <div class="menu_button">小額捐款</div>
+      <div class="menu_button">民眾服務信箱</div>
+    </div>
+    <!-- social media -->
+    <div id="header_social">
+      <div class="social_button"><img src="../assets/icons/social_fb.svg"/></div>
+      <div class="social_button"><img src="../assets/icons/social_ig.svg"/></div>
+      <div class="social_button"><img src="../assets/icons/social_youtube.svg"/></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#header {
+  display: flex;
+  flex-direction: row;
+  border: 1px solid hotpink;
+
+  padding: 16px 24px;
+}
+#header_logo {
+  flex: 0 0 auto;
+  border: 1px solid blue;
+
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+}
+#logo_title {
+  margin-left: 8px;
+}
+
+#header_menu {
+  flex: 1;
+  border: 1px solid green;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+
+.menu_button {
+  padding: 8px;
+  cursor: pointer;
+}
+#header_social {
+  flex: 0 0 auto;
+  border: 1px solid orange;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+}
+.social_button {
+  padding: 2px;
+  cursor: pointer;
+  
+  display: flex;
+  align-items: center;;
+}
+</style>
